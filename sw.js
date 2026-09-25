d7a0c1260cc1111e8f4129ac2db1e5f07bf4d6a1
sweater9 <sweater.9arc@icloud.com>
2026-09-25 22:53:18 +0400

    Add reconciled V2 offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="intellitools-v2-2";
+const ASSETS=["./","./index.html","./v2.css","./tools.js","./v2-tools.js","./manifest.webmanifest","./assets/icon.svg","./vendor/pdf-lib.min.js","./vendor/qrcode.min.js","./vendor/jsqr.min.js"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match("./index.html"))))});
